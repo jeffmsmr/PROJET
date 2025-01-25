ab7f1b6fb73f67c79df6ba12edd7fc42740463cf
Jeffrey Messmer <jeffrey.messmer2@gmail.com>
2025-01-26 00:46:02 +0100

    auth+contact

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,6 +1,15 @@
 import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La Galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/auth/account.html"),
+    new Route("/editPassword", "Modifier mon mot de passe", "/pages/auth/editPassword.html"),
+    new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/mentions", "Mentions", "/pages/mentions.html"),
+
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
